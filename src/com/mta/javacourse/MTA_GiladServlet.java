341284061f10b7c47eead5251b5ed7eef9231533
Gilad <giladda@mail.mta.ac.il>
2014-11-15 18:19:27 +0200

    Exercise 03

--- src/com/mta/javacourse/MTA_GiladServlet.java
+++ src/com/mta/javacourse/MTA_GiladServlet.java
@@ -1,21 +1,50 @@
 package com.mta.javacourse;
 
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class MTA_GiladServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+
+		int num1 = 4;
+		int num2 = 3;
+		int num3 = 7;
+		int result = (num1 + num2) * num3;
+
+		String resStr1 = new String("<h1>Result of --> " + "(" + num1 + "+" + num2 + ")" + "*" + num3 + " = " + result + "</h1>");
+		//calculate area of circle
+		int radius = 50;
+		double circleArea = (radius * radius) * Math.PI;
+		String roundArea = String.format("%.2f", circleArea);
 		
-			int num1 = 4;
-			int num2 = 3;
-			int num3 = 7;
-			int result = (num1+num2)*num3;
+		String line1 = new String ("calculation 1: Area of circle with radius " + radius + " is: " + roundArea + " square-cm.");
 		
-		String resStr = new String ("<h1>Result of --> " + "(" + num1 + "+" + num2 + ")" + "*" + num3 + " = " + result + "</h1>" );
+		
+		//calculate opposite of triangle
+		int hypotenuse = 50;
+		int angleB = 30;
+		double angleBInRadians = Math.toRadians(angleB);
+		double opposite = hypotenuse * angleBInRadians;
+		String roundOpposite = String.format("%.2f", opposite);
+		String line2 = new String ("calculation 2: Length of opposite, where angle B is: " + angleB + " is: " + roundOpposite + ".");
+		
+		//calculate power
+		int base = 20;
+		int exp = 13;
+		long powRes = 1;
+		
+		for (int i = 1; i <= exp; i++)
+		{
+			powRes *= base;
+		}
+		String line3 = new String ("calculation 3: Power of " + base +" with exp of " + exp + " is: " + powRes);
+		
+		String resultStr = resStr1 + "<br>" + line1 + "<br>" + line2 + "<br>" + line3;
 		
 		resp.setContentType("text/html");
-		resp.getWriter().println(resStr);
+		resp.getWriter().println(resultStr);
 	}
 }
