295bf0e9d71beb54073c5c9ad93b7a203ac72bde
Gilad <giladda@mail.mta.ac.il>
2014-12-09 19:50:58 +0200

    Exercise 06

--- src/com/mta/javacourse/model/Portfolio.java
+++ src/com/mta/javacourse/model/Portfolio.java
@@ -1,32 +1,96 @@
 package com.mta.javacourse.model;
 
 import java.util.Date;
-
-import com.mta.javacourse.*;
-
+/**
+ * Class for creating a stock portfolio
+ * 
+ * @author Gilad David
+ *
+ */
 public class Portfolio {
 
 	public final static int MAX_PORFOLIO_SIZE = 5;
 
-	private String title = ("<h1><h1>Portfolio Title</h1>");
-
-	private int portfolioSize = 0;
-
+	private String title;
+	private int portfolioSize;
 	private Stock[] stocks;
 	private StockStatus[] stocksStatus;
 
 	public Portfolio()
 	{
+		portfolioSize = 0;
+		setTitle ("Portfolio #1");
 		stocks = new Stock[MAX_PORFOLIO_SIZE];
 		stocksStatus = new StockStatus[MAX_PORFOLIO_SIZE];
 	}
-
-	public void addStock(Stock s)
+	/**
+	 * cto'r which receives a title
+	 * and calls 1st cto'r for setting it
+	 * 
+	 * @param title
+	 */
+	public Portfolio (String title)
 	{
-		stocks [portfolioSize] = s;
-		portfolioSize++;
+		this ();
+		this.setTitle(title);
+	}
+	/**
+	 * copy c'tor for making instance copies
+	 * of Portfolio object
+	 * 
+	 * @param portfolio
+	 */
+	public Portfolio(Portfolio portfolio)
+	{
+		this(portfolio.getTitle());
+		setPortfolioSize(portfolio.getPortfolioSize());
+
+	     for (int i = 0; i < portfolioSize; i++) 
+             stocks[i] = new Stock(portfolio.getStocks()[i]);
+
+	     for(int i = 0; i < portfolioSize; i++)
+                 stocksStatus[i] = new StockStatus(portfolio.getStocksStatus()[i]);     
 	}
 
+	/**
+	 * function for adding new stock to portfolio
+	 * within portfolio size limits
+	 * 
+	 * @param stock
+	 */
+	public void addStock(Stock stock)
+	{
+		if (portfolioSize < MAX_PORFOLIO_SIZE)
+		{
+			stocks [portfolioSize] = stock;
+			stocksStatus[portfolioSize] = new StockStatus();
+			portfolioSize++;
+		}
+	}
+/**
+ * function for removing a specific stock
+ * from portfolio (according to it's symbol)
+ * 
+ * @param stock
+ */
+    public void removeStock(Stock stock)
+    {
+            for(int i = 0; i < portfolioSize; i++)
+            {
+                    if(this.stocks[i].getSymbol().equals(stock.getSymbol()))
+                    {
+                            if(i != portfolioSize-1 && portfolioSize > 1)
+                                    for(int j = i; j < portfolioSize-1; j++)
+                                    {
+                                            this.stocks[j] = new Stock(this.stocks[j+1]);
+                                    }
+                    }
+            }
+            portfolioSize--;
+    }
+	
+    //Setters & Getters-->
+    
 	public Stock[] getStocks()
 	{
 		return stocks;
@@ -39,10 +103,26 @@ public class Portfolio {
 	public void setStocksStatus(StockStatus[] stocksStatus) {
 		this.stocksStatus = stocksStatus;
 	}
-	
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPortfolioSize() {
+		return portfolioSize;
+	}
+
+	public void setPortfolioSize(int portfolioSize) {
+		this.portfolioSize = portfolioSize;
+	}
+
 	public String getHtmlString()
 	{
-		String res = title;
+		String res = "<h1>" + getTitle() + "</h1>";
 		int i = 0;
 		while (i < portfolioSize)
 		{
@@ -51,7 +131,13 @@ public class Portfolio {
 		}
 		return res;
 	}
-
+/**
+ * an inner class for
+ * storing stock status
+ * 
+ * @author Gilad David
+ *
+ */
 	public class StockStatus {
 
 		final static int DO_NOTHING = 0;
@@ -63,7 +149,37 @@ public class Portfolio {
 		private Date date;
 		private int recommendation;
 		private int stockQuantity;
-
+		
+		/**
+		 * 
+		 * c'tor for initializing
+		 * stock status
+		 */
+		public	StockStatus()
+		{
+			symbol = "empty";
+			currentAsk = 0;
+			currentBid = 0;
+			date = new Date();
+			recommendation = DO_NOTHING;
+			stockQuantity = 0;
+		}
+		/**
+		 * copy c'tor for stock status
+		 * 
+		 * @param stockStatus
+		 */
+		public StockStatus (StockStatus stockStatus)
+		{
+		      this();
+              setSymbol(stockStatus.symbol);
+              setCurrentAsk(stockStatus.currentAsk);
+              setCurrentBid(stockStatus.currentBid);
+              setDate(stockStatus.date);
+              setRecommendation(stockStatus.recommendation);
+              setStockQuantity(stockStatus.stockQuantity);
+		}
+		
 		public String getSymbol() {
 			return symbol;
 		}
--- src/com/mta/javacourse/model/Stock.java
+++ src/com/mta/javacourse/model/Stock.java
@@ -2,48 +2,94 @@ package com.mta.javacourse.model;
 
 import java.util.Date;
 
+/**
+ * Class for setting stock fields
+ * 
+ * @author Gilad David
+ *
+ */
 public class Stock {
-	
+
 	//Data Members
-	private String Symbol;
-	private float Ask;
-	private float Bid;
-	private Date Date;
-	
+	private String symbol;
+	private float ask;
+	private float bid;
+	private Date date;
+/**
+ * c'tor for initializing stocks
+ */
+	public Stock()
+	{
+		symbol = "";
+		ask = 0;
+		bid = 0;
+		date = new Date();
+	}
+
+	/**
+	 * c'tor for filling stock fields with values
+	 * 
+	 * @param symbol
+	 * @param ask
+	 * @param bid
+	 * @param date
+	 */
+	public Stock (String symbol, float ask, float bid, Date date)
+	{
+		this();
+		setSymbol(symbol);
+		setAsk(ask);
+		setBid(bid);
+		setDate(date);
+	}
+
+	/**
+	 * copy c'tor for stock
+	 * @param stock
+	 */
+	public Stock (Stock stock)
+	{
+		this (stock.getSymbol(), stock.getAsk(), stock.getBid(), stock.getDate());
+	}
+
 	//Setters and Getters--->
-	
+
 	//Symbol S&G
 	public String getSymbol() {
-		return Symbol;
+		return symbol;
 	}
+
 	public void setSymbol(String symbol) {
-		Symbol = symbol;
+		this.symbol = symbol;
 	}
-	
+
 	//Ask S&G
 	public float getAsk() {
-		return Ask;
+		return ask;
 	}
+
 	public void setAsk(float ask) {
-		Ask = ask;
+		this.ask = ask;
 	}
-	
+
 	//Bid S&G
 	public float getBid() {
-		return Bid;
+		return bid;
 	}
+
 	public void setBid(float bid) {
-		Bid = bid;
+		this.bid = bid;
 	}
-	
+
 	//Date S&G
 	public Date getDate() {
-		return Date;
+		return date;
 	}
+
 	public void setDate(Date date) {
-		Date = date;
+		this.date = date;
 	}
-	
+
 	public String getHtmlDescription()
 	{
 		String stockHtmlDetailsString = "<b>Stock symbol</b>: " +getSymbol() + "<b> Stock Ask</b>: " +getAsk() + "<b> Bid</b>: " +getBid() + "<b> Stock Date</b>: " +getDate();
